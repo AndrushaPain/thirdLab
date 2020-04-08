b4a4951dfdc1bac219012bcb6368d014bf8721f1
AndrushaPain <andreygoreff@yandex.ru>
2020-04-08 18:33:34 +0800

    Добавил классы под объекты

--- thirdLab/Objects.cs
+++ thirdLab/Objects.cs
@@ -7,5 +7,23 @@ using System.Threading.Tasks;
 
 namespace thirdLab
 {
-   
+    public class Planet
+    {
+        public int radius = 0;
+        public bool atmosphere = false;
+        public int gravity = 0;
+    }
+    public enum StarColor { white, blue, yellow, red, orange }
+    public class Star 
+    {
+        public int density = 0;
+        public StarColor color = StarColor.white;
+        public int temperature = 0;
+    }
+    public class Comet
+    {
+        public int passageThroughSS = 0;
+        public string name = "Comet";
+
+    }
 }
